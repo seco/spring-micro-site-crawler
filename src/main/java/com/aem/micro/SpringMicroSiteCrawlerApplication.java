7b09b6a975173606df7d00c8c86af6f5b624d814
Niranjan <nirpanig@publicisgroupe.net>
2019-09-13 20:57:26 +0530

    some changes

--- src/main/java/com/aem/micro/SpringMicroSiteCrawlerApplication.java
+++ src/main/java/com/aem/micro/SpringMicroSiteCrawlerApplication.java
@@ -3,12 +3,14 @@ package com.aem.micro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 import brave.sampler.Sampler;
 
 @EnableDiscoveryClient
+@EnableHystrix
 @EnableFeignClients("com.aem.micro.controller")
 @SpringBootApplication
 public class SpringMicroSiteCrawlerApplication {
